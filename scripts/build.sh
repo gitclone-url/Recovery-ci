b8748c1b480a17d5122f8dba6488c0a3eb5f4207
Abhijeet <98699436+gitclone-url@users.noreply.github.com>
2023-01-20 02:17:16 +0600

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,14 +3,6 @@
 # Source Configs
 source $CONFIG
 
-# A Function to Send Posts to Telegram
-telegram_message() {
-	curl -s -X POST "https://api.telegram.org/bot${TG_TOKEN}/sendMessage" \
-	-d chat_id="${TG_CHAT_ID}" \
-	-d parse_mode="HTML" \
-	-d text="$1"
-}
-
 # Change to the Source Directry
 cd ~
 
@@ -22,26 +14,6 @@ if [ -z "$CCACHE_SIZE" ]; then
 else
     ccache -M ${CCACHE_SIZE}
 fi
-
-
-
-# Send the Telegram Message
-
-echo -e \
-"
-🦊 OrangeFox Recovery CI
-
-✔️ The Build has been Triggered!
-
-📱 Device: "${DEVICE}"
-🖥 Build System: "${FOX_BRANCH}"
-🌲 Logs: <a href=\"https://cirrus-ci.com/build/${CIRRUS_BUILD_ID}\">Here</a>
-" > tg.html
-
-TG_TEXT=$(< tg.html)
-
-telegram_message "${TG_TEXT}"
-echo " "
 
 # Prepare the Build Environment
 source build/envsetup.sh
