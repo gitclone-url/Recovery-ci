80a493dd594bb9325e4c075812836a6af93829ce
Abhijeet <98699436+gitclone-url@users.noreply.github.com>
2023-01-19 23:17:30 +0600

    Update sync.sh

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -6,6 +6,7 @@ source $CONFIG
 # Change to the Home Directory
 cd ~
 
+
 # Clone the Sync Repo
 repo init --depth=1 -u $TWRP_SYNC -b $TWRP_BRANCH
 repo sync
@@ -13,4 +14,7 @@ repo sync
 # Clone Trees
 DT_PATH="device/${OEM}/${DEVICE}"
 git clone $DT_LINK $DT_PATH || { echo "ERROR: Failed to Clone the Device Trees!" && exit 1; }
+
+
+# Exit
 exit 0
